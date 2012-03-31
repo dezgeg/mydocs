7f445c680b2f6d38832cf115d638525f4419d32e
Tuomas Tynkkynen <dezgeg@gmail.com>
2012-03-31 22:41:23 +0300

    Added check for e-mail uniqueness.

--- mydocs/edit/models.py
+++ mydocs/edit/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django_openid_auth.exceptions import IdentityAlreadyClaimed
 
 class Document(models.Model):
 	name = models.CharField(max_length=64)
@@ -6,3 +10,8 @@ class Document(models.Model):
 
 	def __unicode__(self):
 		return self.name
+
+@receiver(pre_save, sender=User)
+def require_unique_email(sender, instance, **kwargs):
+	if User.objects.filter(email=instance.email).exclude(pk=instance.pk).count() >= 1:
+		raise IdentityAlreadyClaimed("Someone has already registered with this e-mail.")
